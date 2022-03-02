b07602ca282379d7b449cd8a6ccc968b66f120e6
joneheart <tladlf33@naver.com>
2022-03-02 17:20:29 +0900

    eb

--- nst/settings.py
+++ nst/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$v)%3@5m=i60cndk_i(-61q+pwz0%8*0qtwif7k*o58r&@c0&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 # APND_SLASH = True
