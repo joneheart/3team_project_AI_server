920c26fd34f710aebe421ca08b36b7ff1732b793
joneheart <96563227+joneheart@users.noreply.github.com>
2022-04-13 22:08:25 +0900

    Update apps.py

--- nstapp/apps.py
+++ nstapp/apps.py
@@ -7,5 +7,5 @@ class NstappConfig(AppConfig):
     name = 'nstapp'
     hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
     # secret 으로 빼놓고 해당 파일은 .gitignore를 해도 좋지만, github 레포를 그냥 private 으로 설정하는 것을 추천 (배포 과정이 더 간단해짐)
-    s3 = boto3.client('s3', aws_access_key_id='AKIA55DV3VENXHRVJPUD',
-                      aws_secret_access_key='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', region_name='ap-northeast-2')
+    s3 = boto3.client('s3', aws_access_key_id='',
+                      aws_secret_access_key='', region_name='ap-northeast-2')
